48e5aa0dbcfaccb9c4524109be6f9628a04e7b8f
Mounika Harini <146058701+Mounikaharini@users.noreply.github.com>
2026-04-14 13:17:38 +0530

    Create day1intro.py

---
+++ day1intro.py
@@ -0,0 +1,68 @@
+#help("keywords")
+'''
+a = 10
+print(a)
+print(type(a))
+b = 10.01
+print(b)
+print(type(b))
+c = True
+print(c)
+print(type(c))
+d = "Mounika"
+print(d)
+print(type(d))
+e = [1,2,1.978,"Mounika",True]
+print(e)
+print(type(e))
+f = (1,2,1.978,"Mounika",True)
+print(f)
+print(type(f))
+s = {1,2,42,43,6,6,2,3,1,2,1,2,4,6,8}
+print(s)
+print(type(s))
+h = {1:"Name1",2:"Name2",1:"Name3"}
+print(h)
+print(type(h))
+
+#arithmetic operation
+print(10+36)
+a = 10
+b = 10
+print(a+b)
+print(a+21)
+print(320-29)
+print(2*6)
+print(5/2)
+print(5//2)
+print(5%2)
+print(2**3)
+'''
+#assignment operator
+a = 10
+a+=10
+print(a)
+a-=10
+print(a)
+a*=2
+print(a)
+a/=5
+print(a)
+a//=2
+print(int(a))
+a**=2
+print(a)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
