d7306203b62b551eb262fe3e9119571a060e99f8
Mounika Harini <146058701+Mounikaharini@users.noreply.github.com>
2026-04-15 13:25:03 +0530

    Create day2Intro.py

---
+++ day2Intro.py
@@ -0,0 +1,99 @@
+
+#variables
+name = "Mounika"
+phone = 9384542020
+course = "python"
+hours = "60-70"
+print(name)
+print(phone)
+print(course)
+print(hours)
+
+a = 10
+b = 10
+c = a+b
+print(c)
+print(a+b)
+
+#variable task
+
+tamil = 80
+english = 65
+maths = 89
+science = 47
+social = 75
+total = tamil + english + maths + science + social
+print("Total :",total/5)
+
+help("keywords")
+
+#datatypes
+a = 10
+print(a)
+print(type(a))
+
+a = 10.528541
+print(a)
+print(type(a))
+
+a = True
+print(a)
+print(type(a))
+
+a = "Python Class"
+print(a)
+print(type(a))
+
+a = [1,1,2,3,4.67,"mounika"]
+print(a)
+print(type(a))
+
+a = (1,1,2,3,4.67,"mounika")
+print(a)
+print(type(a))
+
+a = {67,2,3,4.67,"mounika",1,1}
+print(a)
+print(type(a))
+
+a={1:"Rajpriyan",2:"Messi",3:"Dhoni",4:"Vishnu",1:"Madan Gowri"}
+print(a)
+print(type(a))
+
+#arithmetic operator
+print(23+36)
+print(25-15)
+print(3*6)
+print(5/2)
+print(5//2)
+print(5%2)
+print(2**3)
+
+#assignment operator
+a = 10
+print(a)
+
+a+=10
+print(a)
+
+a-=10
+print(a)
+
+a*=2
+print(a)
+
+a/=2
+print(a)
+
+a//=1
+print(a)
+
+a%=2
+print(a)
+
+a**=2
+print(a)
+
+
+
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                              
